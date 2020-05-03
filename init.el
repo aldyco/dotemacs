ea3b8d910ea5ffd55bea2431958ddc76493f529a
Aldyco Turang <me@aldyco.com>
2020-05-03 22:14:17 +0700

    start using dotemacs.org

---
+++ init.el
@@ -0,0 +1,35 @@
+;;; init.el --- My Emacs configuration
+
+;;; Commentary:
+
+;; Following lines load an Org file and build the configuration code out of it.
+
+;;; Code:
+
+(let ((gc-cons-threshold most-positive-fixnum))
+
+  ;; Set repositories
+  (require 'package)
+  (setq-default
+   load-prefer-newer t
+   package-enable-at-startup nil)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (package-initialize)
+
+  ;; Install dependencies
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package t))
+  (setq-default
+   use-package-always-defer t
+   use-package-always-ensure t)
+
+  ;; Use latest Org
+  (use-package org :ensure org-plus-contrib)
+
+  ;; Tangle configuration
+  (org-babel-load-file (expand-file-name "dotemacs.org" user-emacs-directory))
+  (garbage-collect))
+
+;;; init.el ends here
