19e142e53b85f0f50e905ebb4631d73bade4ac5a
Aldyco Turang <me@aldyco.com>
2020-05-07 16:32:09 +0700

    add melpa stable and ensure system package

--- init.el
+++ init.el
@@ -14,8 +14,15 @@
    load-prefer-newer t
    package-enable-at-startup nil)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
+
+  ;; Set explicit priorities (gnu is the default package-archive)
+  (setq package-archive-priorities '(("org" . 4)
+									 ("melpa" . 3)
+									 ("melpa-stable" . 2)
+									 ("gnu" . 1)))
 
   ;; Install dependencies
   (unless (package-installed-p 'use-package)
@@ -24,6 +31,9 @@
   (setq-default
    use-package-always-defer t
    use-package-always-ensure t)
+
+  (use-package use-package-ensure-system-package
+	:ensure t)
 
   ;; Use latest Org
   (use-package org :ensure org-plus-contrib)
